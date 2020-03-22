064e64bdb861510919cd649262634188a7a95ee6
adamboov <48423629+adamboov@users.noreply.github.com>
2020-03-22 22:54:01 +0800

    初始化项目

---
+++ src/com/adam/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.adam.dao;
+
+import com.adam.entity.User;
+
+/**
+ * 
+ * @author VAIO-Adam
+ *
+ */
+public class UserDao {
+	public User queryUser(User user) {
+		return user;
+		
+	}
+}
---
+++ src/com/adam/service/UserService.java
@@ -0,0 +1,13 @@
+package com.adam.service;
+
+import com.adam.dao.UserDao;
+
+/**
+ * 
+ * @author VAIO-Adam
+ *
+ */
+public class UserService {
+	UserDao userdao = new UserDao();
+	
+}
